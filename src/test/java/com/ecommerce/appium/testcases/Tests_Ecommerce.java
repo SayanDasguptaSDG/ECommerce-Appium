1a62fcfc4cbf695167d6c7a8e48ccca7d3d1e7a8
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-09-04 09:44:11 +0530

    toast message validation

--- src/test/java/com/ecommerce/appium/testcases/Tests_Ecommerce.java
+++ src/test/java/com/ecommerce/appium/testcases/Tests_Ecommerce.java
@@ -2,6 +2,7 @@ package com.ecommerce.appium.testcases;
 
 import com.ecommerce.appium.helper.BaseTest;
 import io.appium.java_client.AppiumBy;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Tests_Ecommerce extends BaseTest {
@@ -15,5 +16,16 @@ public class Tests_Ecommerce extends BaseTest {
         driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Argentina\"]")).click();
         driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop")).click();
         Thread.sleep(3000);
+    }
+
+    @Test
+    public void verify_toast_message() throws InterruptedException {
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/radioMale")).click();
+        driver.findElement(AppiumBy.id("android:id/text1")).click();
+        scrollTillFound("Argentina");
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Argentina\"]")).click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        String toastMessage = driver.findElement(AppiumBy.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+        Assert.assertEquals(toastMessage, "Please enter your name");
     }
 }
